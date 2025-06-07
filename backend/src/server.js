c5645ef38cd15d4e0a51f4bd8c01ce57fcce4a97
Aniruddha Chatterjee <anichat1999@gmail.com>
2025-06-07 09:49:53 +0530

    Initial Project Setup Done

---
+++ backend/src/server.js
@@ -0,0 +1,18 @@
+import express from "express";
+import { config } from "dotenv";
+import authRoutes from "./routes/auth.route.js";
+import {connectDB} from "./lib/db.js";
+
+config();
+
+const app=express();
+const PORT=process.env.PORT || 5001;
+
+
+app.use("/api/auth",authRoutes);
+
+app.listen(PORT,()=>{
+    console.log(`Server is running at port:${PORT}...`);
+    connectDB();
+});
+
